b2313e8fb0e6977bf607b2015f4bc5ced6d5fbdb
haya <hayasoft@hotmail.com>
2015-09-07 17:40:11 +0900

    1.3.0 release

--- tsdump.h
+++ tsdump.h
@@ -1,5 +1,5 @@
-#define VERSION_STR				"1.3.0-dev"
-#define DATE_STR				"2015/x/x"
+#define VERSION_STR				"1.3.0"
+#define DATE_STR				"2015/09/07"
 
 #define BUFSIZE_DEFAULT			96
 #define OVERLAP_SEC_DEFAULT		15
