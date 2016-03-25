1dbd47008d9fb7285d4f66a62a9e0502aaa4d63f
haya <hayasoft@hotmail.com>
2016-03-25 20:53:26 +0900

    stdio版ファイル出力モジュールの記述を削除

--- modules.h
+++ modules.h
@@ -4,7 +4,6 @@ extern module_def_t mod_core;
 extern module_def_t mod_bondriver;
 extern module_def_t mod_b25decoder;
 extern module_def_t mod_fileoutput_win;
-extern module_def_t mod_fileoutput_stdio;
 extern module_def_t mod_pipeoutput_win;
 extern module_def_t mod_cmdexec_win;
 extern module_def_t mod_path_resolver;
@@ -15,7 +14,6 @@ module_def_t *static_modules[] = {
 	&mod_bondriver,
 	&mod_b25decoder,
 	&mod_fileoutput_win,
-	//&mod_fileoutput_stdio,
 	&mod_pipeoutput_win,
 	&mod_cmdexec_win,
 	&mod_path_resolver,
