7200b4dedea877da4ddf91ac663057a75c90ba0c
haya <hayasoft@hotmail.com>
2016-08-02 09:01:11 +0900

    デフォルトデコーダの機能をtsパケットのアライメント調整フィルタとして独立

--- core/default_decoder.c
+++ core/default_decoder.c
@@ -12,6 +12,7 @@
 #include "utils/arib_proginfo.h"
 #include "core/module_hooks.h"
 #include "utils/ts_parser.h"
+#include "core/default_decoder.h"
 
 int64_t ts_n_drops = 0;
 int64_t ts_n_total = 0;
@@ -53,47 +54,69 @@ void ts_statics_counter(ts_header_t *tsh)
 	ts_counter[tsh->pid] = tsh->continuity_counter + 16; /* offset 16 */
 }
 
-/* 188バイトアラインではないストリームを送ってくるBonDriver（たとえばFriio）に対応するためのダミーデコーダ */
-void default_decoder(unsigned char **decbuf, int *n_decbuf, const unsigned char *buf, int n_buf)
+void create_ts_alignment_filter(ts_alignment_filter_t *filter)
 {
-	static unsigned char *tmpbuf = NULL;
-	static int n_tmpbuf = 0;
-	static unsigned char rem[188];
-	static int n_rem = 0;
+	filter->buf_size = 188 * 1024;
+	filter->buf = (uint8_t*)malloc(filter->buf_size);
+	filter->remain = 0;
+	filter->bytes = 0;
+	filter->skip = 0;
+}
 
-	if (n_buf == 0) {
-		*decbuf = NULL;
-		*n_decbuf = 0;
-		return;
-	}
+void delete_ts_alignment_filter(ts_alignment_filter_t *filter)
+{
+	free(filter->buf);
+}
 
-	int n = n_rem + n_buf;
+void ts_alignment_filter(ts_alignment_filter_t *filter, uint8_t **out_buf, int *out_bytes, const uint8_t *in_buf, int in_bytes)
+{
+	uint8_t tmp[188];
+	int bytes, skip;
 
-	if (n > n_tmpbuf) {
-		if (tmpbuf != NULL) {
-			free(tmpbuf);
+	bytes = filter->remain + in_bytes;
+	if (bytes > filter->buf_size) {
+		if (filter->remain > 0) {
+			memcpy(tmp, &filter->buf[filter->bytes], filter->remain);
 		}
-		tmpbuf = (unsigned char*)malloc(n);
-		n_tmpbuf = n;
+		free(filter->buf);
+		while (bytes > filter->buf_size) {
+			filter->buf_size *= 2;
+		}
+		filter->buf = (uint8_t*)malloc(filter->buf_size);
+		if (filter->remain > 0) {
+			memcpy(filter->buf, tmp, filter->remain);
+		}
+	} else if (filter->remain > 0) {
+		memmove(filter->buf, &filter->buf[filter->bytes], filter->remain);
+	}
+	memcpy(&filter->buf[filter->remain], in_buf, in_bytes);
+
+	skip = 0;
+	while (filter->buf[skip] != 0x47 && skip < bytes && skip < 188) {
+		skip++;
+	}
+	filter->bytes = bytes;
+	filter->skip = skip;
+
+	bytes -= skip;
+	*out_bytes = bytes / 188 * 188;
+	*out_buf = &filter->buf[skip];
+	filter->remain = bytes - (*out_bytes);
+}
+
+/* 188バイトアラインではないストリームを送ってくるBonDriver（たとえばFriio）に対応するためのダミーデコーダ */
+void default_decoder(uint8_t **out_buf, int *out_bytes, const uint8_t *in_buf, int in_bytes)
+{
+	static ts_alignment_filter_t filter;
+	static int init_filter = 0;
+
+	if (!init_filter) {
+		create_ts_alignment_filter(&filter);
+		init_filter = 1;
 	}
 
-	memcpy(tmpbuf, rem, n_rem);
-	memcpy(&tmpbuf[n_rem], buf, n_buf);
-
-	int i = 0;
-	while (tmpbuf[i] != 0x47 && i < n && i < 188) {
-		i++;
-	}
-
-	int n_dec = (n - i) / 188 * 188;
-
-	n_rem = n - i - n_dec;
-	memcpy(rem, &tmpbuf[i + n_dec], n_rem);
-
-	*decbuf = &tmpbuf[i];
-	*n_decbuf = n_dec;
-
-	if (i != 0) {
-		output_message(MSG_WARNING, TSD_TEXT("skipped %d bytes"), i);
+	ts_alignment_filter(&filter, out_buf, out_bytes, in_buf, in_bytes);
+	if (filter.skip != 0) {
+		output_message(MSG_WARNING, TSD_TEXT("skipped %d bytes"), filter.skip);
 	}
 }
--- core/default_decoder.h
+++ core/default_decoder.h
@@ -2,5 +2,17 @@ extern int64_t ts_n_drops;
 extern int64_t ts_n_total;
 extern int64_t ts_n_scrambled;
 
+typedef struct
+{
+	int remain;
+	int skip;
+	int bytes;
+	int buf_size;
+	uint8_t *buf;
+} ts_alignment_filter_t;
+
 void ts_statics_counter(ts_header_t *tsh);
-void default_decoder(unsigned char **decbuf, int *n_decbuf, const unsigned char *buf, int n_buf);
+void create_ts_alignment_filter(ts_alignment_filter_t *filter);
+void delete_ts_alignment_filter(ts_alignment_filter_t *filter);
+void ts_alignment_filter(ts_alignment_filter_t *filter, uint8_t **out_buf, int *out_bytes, const uint8_t *in_buf, int in_bytes);
+void default_decoder(uint8_t **out_buf, int *out_bytes, const uint8_t *in_buf, int in_bytes);
